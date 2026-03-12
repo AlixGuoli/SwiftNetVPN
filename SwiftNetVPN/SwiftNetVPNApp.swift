9fa6ac53f16ba5ba17a24bfed6924f82ba5505d2
AlixGuoli <1425300103@qq.com>
2026-03-12 15:51:41 +0800

    Add iOS app target

---
+++ SwiftNetVPN/SwiftNetVPNApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNetVPNApp.swift
+//  SwiftNetVPN
+//
+//  Created by zying on 2026/3/11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftNetVPNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
